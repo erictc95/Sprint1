04f192c19e9a8e404e62e4323226eb3d203224aa
erictc95 <maxerictc95@hotmail.com>
2026-02-04 13:11:57 +0100

    Version3 Exercisi2 - Coches

--- Nivel-1/Ejercicio2/src/main/java/cars/CarMain.java
+++ Nivel-1/Ejercicio2/src/main/java/cars/CarMain.java
@@ -8,6 +8,10 @@ public class CarMain {
         Car car1 = new Car("Gallardo", 500);
         car1.accelerate();
         System.out.println(car1);
+        Car car2 = new Car("Aventador", 600);
+        Car car3 = new Car("Urus", 450);
+        System.out.println(car2);
+        System.out.println(car3);
     }
 
 }
