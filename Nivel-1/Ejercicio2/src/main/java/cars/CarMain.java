cb2777ca02517fd71d09f7ae5faaccae832d67a5
erictc95 <maxerictc95@hotmail.com>
2026-02-04 13:05:10 +0100

    Version2 Exercisi2 - Coches

--- Nivel-1/Ejercicio2/src/main/java/cars/CarMain.java
+++ Nivel-1/Ejercicio2/src/main/java/cars/CarMain.java
@@ -1,4 +1,13 @@
 package cars;
 
+import static cars.Car.brake;
+
 public class CarMain {
+    public static void main(String[] args) {
+        Car.brake();
+        Car car1 = new Car("Gallardo", 500);
+        car1.accelerate();
+        System.out.println(car1);
+    }
+
 }
